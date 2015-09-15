97e971d5ea72480a2e40be48f9f8097917445026
Andrew <andrew@homeemail.me.uk>
2015-09-15 20:44:09 +0100

    added fixed length dispense

--- SolderDispenser.ino
+++ SolderDispenser.ino
@@ -5,7 +5,7 @@ int SOLENOID2_PIN = 14;
 
 int SOLENOID_DELAY = 100;
 
-long DEBOUNCE_TIME = 200;
+long DEBOUNCE_TIME = 500;
 long lastChangeTime = 0;
 
 int out1State = LOW;
@@ -13,12 +13,19 @@ int out2State = LOW;
 
 int lastInput = LOW;
 
+int OPEN_TIME = 300; 			// length of open delay
+int dispenseLength = 1;			// multiplier for length
+
 
 void setup()
 {
 	pinMode(SWITCH_PIN, OUTPUT);
 	pinMode(SOLENOID1_PIN, OUTPUT);
 	pinMode(SOLENOID2_PIN, OUTPUT);
+
+	// initialise the solenoids to both being closed.  assumes normally closed valves
+	digitalWrite(SOLENOID1_PIN, LOW);
+	digitalWrite(SOLENOID2_PIN, LOW);
 
 	lastChangeTime = millis();
 }
@@ -30,14 +37,27 @@ void loop()
 
 	if (input == HIGH ){
 		// new press of the button
+		// assumes we want a metered amount
 		if (lastInput == LOW && millis() - lastChangeTime > DEBOUNCE_TIME){
 			Serial.print(" - Button pressed");
 
-			digitalWrite(SOLENOID2_PIN, HIGH);
-			delay(SOLENOID_DELAY);
-			digitalWrite(SOLENOID1_PIN, HIGH);
+			digitalWrite(SOLENOID2_PIN, LOW);		// close the back solenoid
+			//delay(SOLENOID_DELAY);				// wait for this
+			digitalWrite(SOLENOID1_PIN, HIGH);		// open the front one, lets the air flow
+
+			delay(OPEN_TIME * dispenseLength);		// wait for the flow
+
+			// purge the air
+			digitalWrite(SOLENOID1_PIN, LOW);		// close the front solenoid
+			digitalWrite(SOLENOID2_PIN, HIGH);		// close the back solenoid
+			delay(SOLENOID_DELAY);					// wait
+			digitalWrite(SOLENOID2_PIN, LOW);		// close the back valve
+
 
 			//lastChangeTime = millis();
+		}
+		else {
+			Serial.print(" - Button debounce - wait longer!");
 		}
 	}
 
@@ -45,9 +65,9 @@ void loop()
 	if (input == LOW && lastInput == HIGH){
 		Serial.print(" - Button released");
 
-		digitalWrite(SOLENOID1_PIN, LOW);
-		delay(SOLENOID_DELAY);
-		digitalWrite(SOLENOID2_PIN, LOW);
+//		digitalWrite(SOLENOID1_PIN, LOW);
+//		delay(SOLENOID_DELAY);
+//		digitalWrite(SOLENOID2_PIN, LOW);
 
 		lastChangeTime = millis();
 	}
